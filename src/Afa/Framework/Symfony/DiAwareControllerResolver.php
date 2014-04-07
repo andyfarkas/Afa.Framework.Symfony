c00f0009018ac294e5c749ce6a01b4ee5a783792
Andy Farkas <afarky@gmail.com>
2014-04-07 16:25:52 +0200

    Removed dependency on symfony request object

--- src/Afa/Framework/Symfony/DiAwareControllerResolver.php
+++ src/Afa/Framework/Symfony/DiAwareControllerResolver.php
@@ -44,4 +44,39 @@ class DiAwareControllerResolver extends \Symfony\Component\HttpKernel\Controller
 
         return array($controller, $method);
     }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $symfonyRequest
+     * @param $controller
+     * @param array $parameters
+     * @return array
+     * @throws \RuntimeException
+     */
+    protected function doGetArguments(\Symfony\Component\HttpFoundation\Request $symfonyRequest, $controller, array $parameters)
+    {
+        $attributes = $symfonyRequest->attributes->all();
+        $arguments = array();
+        $request = new Request($symfonyRequest);
+        foreach ($parameters as $param) {
+            if (array_key_exists($param->name, $attributes)) {
+                $arguments[] = $attributes[$param->name];
+            } elseif ($param->getClass() && $param->getClass()->isInstance($request)) {
+                $arguments[] = $request;
+            } elseif ($param->isDefaultValueAvailable()) {
+                $arguments[] = $param->getDefaultValue();
+            } else {
+                if (is_array($controller)) {
+                    $repr = sprintf('%s::%s()', get_class($controller[0]), $controller[1]);
+                } elseif (is_object($controller)) {
+                    $repr = get_class($controller);
+                } else {
+                    $repr = $controller;
+                }
+
+                throw new \RuntimeException(sprintf('Controller "%s" requires that you provide a value for the "$%s" argument (because there is no default value or because there is a non optional argument after this one).', $repr, $param->name));
+            }
+        }
+
+        return $arguments;
+    }
 }
--- src/index.php
+++ src/index.php
@@ -2,6 +2,16 @@
 
 require_once dirname(__DIR__ ) . '/vendor/autoload.php';
 
+
+class MyController
+{
+    public function helloAction(\Afa\Framework\IRequest $request)
+    {
+        var_dump($request);
+    }
+
+}
+
 $app = new \Afa\Framework\Symfony\Application(dirname(__DIR__));
 $app->run();
 
